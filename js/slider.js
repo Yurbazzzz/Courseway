b22c7d81c043bcf27db56cacbf7a5507de26c1ee
Yurbazzzz <74986111+Yurbazzzz@users.noreply.github.com>
2021-11-28 01:31:16 +0200

    page-proofs done

---
+++ js/slider.js
@@ -0,0 +1,8 @@
+$('#slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    appendArrows: 0,
+    prevArrow: $('.slick-prev'),
+    nextArrow: $('.slick-next'),
+  });
